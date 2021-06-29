9313606e235e76da744febd8046e15d033b7e459
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-06-28 22:33:27 -0400

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import uuid from 'uuid'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createStore } from 'redux'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Acction
+const buildHabit = ({
+  description = '',
+  streak = 0,
+  setHours = '00:00',
+  setDays = 'everyday',
+  endTime = 0,
+  duration = 0
+} = {}) => ({
+  type: 'BUILD_HABIT',
+  habit: {
+    id: uuid(),
+    description,
+    streak,
+    setHours,
+    setDays,
+    endTime,
+    duration
+  }
+})
+
+const trackHabit = ({ id }) => ({
+  type: 'TRACK_HABIT',
+  id
+})
+
+// Reducer
+const habitReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'BUILD_HABIT':
+      return [
+        ...state,
+        action.habit
+      ]
+    case 'TRACK_HABIT':
+      return state.map((habit) => {
+        return habit.id === action.id
+      })
+    default:
+      return state
+  }
+}
+
+// Store
+const store = createStore(habitReducer)
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(buildHabit({
+  description: 'Workout',
+  setHours: '15:00',
+  duration: 60
+}))
+
+store.dispatch(buildHabit({
+  description: 'Reading',
+  setHours: '06:00',
+  duration: 45
+}))
+
+// store.dispatch(trackHabit())
+
+
+
+const jsx = (
+  <div>
+    <h1>Habit Builders</h1>
+  </div>
+)
+
+
+ReactDOM.render(jsx, document.getElementById('root'));
+
